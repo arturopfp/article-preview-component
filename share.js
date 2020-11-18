009d1a93304c8204b4cad0b171153d67deefa37d
ArturoP <arturoperezfigueroa@gmail.com>
2020-11-18 09:19:13 -0600

    desktop design completed

--- share.js
+++ share.js
@@ -1,7 +1,13 @@
 const footer = document.getElementById('footer-container');
 const shareContainer = document.getElementById('shareContainer');
+const w = window.innerWidth;
 
 const share = () => {
+    if(w < 600) {
     footer.classList.add('display-none');
     shareContainer.classList.remove('display-none');
+    } else {
+      shareContainer.classList.remove('display-none');
+    }
 }
+
